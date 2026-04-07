08c9b6ec90fd5487a9746f81ddfaeaddaff9394a
송화섭 <wassup0783@gmail.com>
2026-04-07 17:50:04 +0900

    chore: update Homebrew formula to v1.6.2

--- Formula/easy-git.rb
+++ Formula/easy-git.rb
@@ -1,8 +1,8 @@
 class EasyGit < Formula
   desc "Interactive CLI tool to make Git easier to use with multi-language support"
   homepage "https://github.com/wassupss/homebrew-easy-git-cli"
-  url "https://registry.npmjs.org/@wassupsong/easy-git-cli/-/easy-git-cli-1.6.1.tgz"
-  sha256 "68cf3da563b16057ace6d155b82ba0236fb487e7a41db665b1ae3ad631d486b6"
+  url "https://registry.npmjs.org/@wassupsong/easy-git-cli/-/easy-git-cli-1.6.2.tgz"
+  sha256 "6a0107402732894cd248ee7478ed0e2f178d65b0c6cfec17d27ac6dae0d3206f"
   license "MIT"
 
   depends_on "node"
